318513d60f2a8fb850cf18e3d1334ff58277cb00
Negner1 <tuguldurbatkhuyag89@gmail.com>
2026-05-05 13:29:09 +0800

    category find by id update

--- src/main/java/mn/icode/controller/CategoryController.java
+++ src/main/java/mn/icode/controller/CategoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,20 +15,30 @@ import mn.icode.repository.CategoryRepository;
 @Controller
 @RequestMapping("/api")
 public class CategoryController {
+
     private final CategoryRepository categoryRepository;
 
     public CategoryController(
-        CategoryRepository categoryRepository) {
+            CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
-@GetMapping("/categories")
+    @GetMapping("/categories")
     public ResponseEntity<List<Category>> getCategories(
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "20") int size
-
     ) {
         int offset = (page - 1) * size;
         return ResponseEntity.ok(categoryRepository.findAll(size, offset));
     }
+
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<Category> getCategoryFilms(@PathVariable("id") int id) {
+        return categoryRepository.findCategoryById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    // @GetMapping("/categories/{id}/films")
+    // public ResponseEntity<FilmTitle> filmsByCategory(){
+    //     return 
+    // }
 }
---
+++ src/main/java/mn/icode/model/FilmTitle.java
@@ -0,0 +1,14 @@
+package mn.icode.model;
+
+public class FilmTitle {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
