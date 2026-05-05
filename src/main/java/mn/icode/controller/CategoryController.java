69da03683c8e898d6de0b06198e7b5224a3271cd
Negner1 <tuguldurbatkhuyag89@gmail.com>
2026-05-05 12:08:54 +0800

    update controler

---
+++ src/main/java/mn/icode/controller/CategoryController.java
@@ -0,0 +1,10 @@
+package mn.icode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class CategoryController {
+    
+}
