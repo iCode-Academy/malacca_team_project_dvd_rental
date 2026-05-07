5698a4c212395bcd4d1edaa7520f7db4a5074763
Negner1 <tuguldurbatkhuyag89@gmail.com>
2026-05-07 10:58:49 +0800

    create method category

--- src/main/java/mn/icode/controller/CategoryController.java
+++ src/main/java/mn/icode/controller/CategoryController.java
@@ -2,10 +2,13 @@ package mn.icode.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -43,5 +46,15 @@ public class CategoryController {
                                         @PathVariable("id") int categoryId){
         List<FilmTitle> films = categoryRepository.filmsByCategory(categoryId);
         return ResponseEntity.ok(films);
+    }
+
+    @PostMapping
+    @RequestMapping("/categories")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        if (category.getCategoryId() == null || category.getName() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        Category created = categoryRepository.create(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 }
