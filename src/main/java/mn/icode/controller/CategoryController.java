352d6a8b3d888c80061e4af765e7e49a336b1bc0
Negner1 <tuguldurbatkhuyag89@gmail.com>
2026-05-07 12:19:28 +0800

    aaa

--- src/main/java/mn/icode/controller/CategoryController.java
+++ src/main/java/mn/icode/controller/CategoryController.java
@@ -3,14 +3,19 @@ package mn.icode.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import mn.icode.model.Category;
+import mn.icode.model.FilmTitle;
 import mn.icode.repository.CategoryRepository;
 
 @Controller
