fa4d58a21e63f31f580a23865f56ad6d6eea6ba9
Zolbayar <zolbayar.m@cbs.mn>
2026-05-14 10:07:15 +0800

    udpate dist remove

--- frontend/dist/api.js
+++
@@ -1,7 +0,0 @@
-export const API_BASE = "http://localhost:8080";
-export async function apiFetch(path) {
-    const res = await fetch(`${API_BASE}${path}`);
-    if (!res.ok)
-        throw new Error(`HTTP ${res.status}: ${res.statusText}`);
-    return res.json();
-}
